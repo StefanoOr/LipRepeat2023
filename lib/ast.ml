fd6e95ee0d554e57a04b41ee6236149859d20fe1
Leonardo Pisano <leonardo.pisano@live.it>
2023-01-30 16:15:56 +0100

    Update ast.ml

--- lib/ast.ml
+++ lib/ast.ml
@@ -18,24 +18,30 @@ type expr =
 type cmd =
   | Skip
   | Break
-  | Assign of string * expr
-  | Assign_arr of string * expr * expr
+  | Assign of ide * expr
+  | Assign_arr of ide * expr * expr
   | Seq of cmd * cmd
   | Repeat of cmd
+  | Repeat_seq of cmd * cmd
   | If of expr * cmd * cmd
-  | Block of cmd
-  | Call of ide * expr
+  | Block of declv * cmd
+  | Call of ide * param
 
-type decl_v =
-  | NullVar
+type declv =
+  | EmptyDecl
   | IntArr of ide * int
   | IntVar of ide
-  | DVSeq of decl_v * decl_v
+  | DVSeq of declv * declv
+  
+type paramf = 
+  | Val of ide
+  | Ref of ide
+
+type parama = expr
 
 (*DICHIARAZIONE DI PROCEDURE*)
-type decl_p =                       
-  | NullProc                      
-  | Proc of ide * par_f * cmd   
-  | DPSeq of decl_p * decl_p
+type declp = Proc of ide * paramf * cmd
 
-type prog = Prog of decl * cmd
+type declplist = DeclList of declp list
+
+type prog = Prog of declv * declplist * cmd
